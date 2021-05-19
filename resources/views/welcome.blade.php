5195698c744ee1ec993c9a9c29c64a96fd1497a1
Alisher <developer.alisher@gmail.com>
2021-05-19 11:58:00 +0600

    f

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,363 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="Натяжные потолки любой сложности">
+    <meta name="keywords" content="Натяжные потолки любой сложности">
+    <meta name="author" content="W-KZ" />
+    <title>Натяжные потолки в Алматы</title>
+    <noindex>
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,300;0,400;0,600;0,900;1,100;1,200&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link href="css/util.css" rel="stylesheet">
+        <link href="css/style.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+        <script src="js/wow.min.js"></script>
+        <script>
+            new WOW().init();
+        </script>
+    </noindex>
+</head>
+
+<body>
+    
+
+
+    <a href="https://wa.me/77052400203?text=Здравствуйте! Я по поводу натяжных потолков" target="_blank" id="wa__toggle">
+        <div class="circlephone" style="transform-origin: center;"></div>
+        <div class="circle-fill" style="transform-origin: center;"></div>
+        <div class="img-circle" style="transform-origin: center;">
+            <div class="img-circleblock" style="transform-origin: center;"></div>
+        </div>
+    </a>
+
+
+    <section id="home" class="pt140 pb80 d-none d-md-block">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-xs-12 mt100 wow animate__animated animate__fadeIn" align="center">
+                    <img src=""  class="mb30">
+
+                    <h1 class="wkz-font-30 bold font-up whiteF">Качественные натяжные потолки в Алматы</h1>
+
+
+                    <p class="wkz-font-20 whiteF mt10" style="font-weight: 300;">«Потолки любой сложности под ключ»</p>
+                    <p class="wkz-font-18  whiteF mt10">от 1300 тт за 1 м<sup>2</sup></p>
+                    <div>
+                        <a href="tel:87073770896" target="_blank" class="btn btn-lg buttonz mt30 ">
+                            <p class="wkz-font-25" style="font-weight: 300;">Позвонить: +7 (747) 557 1973</p>
+                        </a>
+                    </div>
+                    <div align="center">
+                        <a href="tel:87052400203" target="_blank" class="btn btn-lg buttonz mt10 mb10">
+                            <p class="wkz-font-16" style="font-weight: 300;">Заказать</p>
+                        </a>
+                    </div>
+
+                    <p class="wkz-font-18 font-weight-light whiteF mt10">Приедим к Вам, и сделаем замеры бесплатно! </p>
+                    <p class="wkz-font-12 font-weight-light whiteF mt10">Выезд в область по договоренности</p>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="home-mob" class="pt70 pb80 d-xs-block d-md-none">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-xs-12 mt100" align="center">
+
+                    <div align="center"><img src="rlogo.png" width="200px"></div>
+
+                    <h1 class="wkz-font-25 bold font-up whiteF mt10">Качественные натяжные потолки в Алматы</h1>
+                    <p class="wkz-font-14 whiteF mt10" style="font-weight: 300;">«Потолки любой сложности под ключ»</p>
+
+
+                    <div align="center">
+                        <a href="tel:87073770896" target="_blank" class="btn btn-lg buttonz mt30">
+                            <p class="wkz-font-18" style="font-weight: 300;">Позвонить +7 (747) 557 1973</p>
+                        </a>
+                    </div>
+                    <div align="center">
+                        <a href="tel:87052400203" target="_blank" class="btn btn-lg buttonz mt10 ">
+                            <p class="wkz-font-16" style="font-weight: 300;">Заказать</p>
+                        </a>
+                    </div>
+
+                    <p class="wkz-font-14 font-weight-light whiteF mt10" style="font-weight: 300;">Приедим к Вам, и сделаем замеры бесплатно! </p>
+                    <p class="wkz-font-12 font-weight-light whiteF mt5">Выезд в область по договоренности</p>
+                </div>
+            </div>
+        </div>
+    </section>
+    <section id="works2" class="pb150 pt40">
+        <div class="container">
+            <div class="row pad" align="center">
+                <div class="col-md-12 col-xs-12">
+                    <div class="wkz-font-30 bold font-up">Почему мы?</div>
+                    <p class="wkz-font-16" style="color: #333; font-weight: 300;text-transform: none">Мы поможем подобрать решение по оформлению любого помещения.</p>
+                </div>
+            </div>
+
+            <div class="row pad" align="center">
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                    <img src="img/1.png" class="mb20 mt30" width="100px">
+                    <p class="wkz-font-14" style="font-weight: 300;text-transform: none">Успешно устанавливаем потолки<br> более 10 лет!</p>
+                </div>
+
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                    <img src="img/3.png" class="mb20 mt30" width="100px">
+                    <p class="wkz-font-14" style="font-weight: 300;text-transform: none">Инженер приедит к Вам,<br> бесплатно измерит всё и назовет цену.</p>
+
+                </div>
+
+                <div class="col-md-4 col-sm-12 col-xs-12">
+
+                    <img src="img/2.png" class="mb20 mt30" width="100px">
+                    <p class="wkz-font-14" style="font-weight: 300;text-transform: none">У нас есть решения под любой бюджет <br class="d-xs-block d-md-none">от эконом до премиум-уровня.</p>
+                </div>
+
+
+            </div>
+
+
+
+
+
+
+        </div>
+
+    </section>
+    <section id="works" class="pb100 pt100">
+        <div class="container">
+            <div class="row pad" align="center">
+                <div class="col-md-12 col-xs-12">
+                    <div class="wkz-font-30 bold font-up whiteF">ВИДЫ ПОТОЛКОВ</div>
+                    <p class="wkz-font-16" style="color: #fff; font-weight: 300;text-transform: none">Оживите интерьер своей квартиры!</p>
+                </div>
+            </div>
+
+            <div class="row mt20" align="center">
+                <div class="col-md-3 col-sm-12 col-xs-12">
+                    <a data-fancybox="works" href="line/9b.jpg"><img src="line/9b.jpg" class="mb20 mt20" width="270px"></a>
+                    <p class="wkz-font-16 whiteF mb20" style="font-weight: 300;text-transform: none">ДВУХУРОВНЕВЫЕ ПОТОЛКИ</p>
+                </div>
+
+
+                <div class="col-md-3 col-sm-12 col-xs-12">
+                    <a data-fancybox="works" href="line/10b.jpg"><img src="line/10b.jpg" class="mb20 mt20" width="270px"></a>
+                    <p class="wkz-font-16 whiteF mb20" style="font-weight: 300;text-transform: none">ТКАНЕВЫЕ ПОТОЛКИ</p>
+                </div>
+
+                <div class="col-md-3 col-sm-12 col-xs-12">
+                    <a data-fancybox="works" href="line/11b.jpg"><img src="line/11b.jpg" class="mb20 mt20" width="270px"></a>
+                    <p class="wkz-font-16 whiteF mb20" style="font-weight: 300;text-transform: none">САТИНОВЫЕ ПОТОЛКИ</p>
+                </div>
+
+                <div class="col-md-3 col-sm-12 col-xs-12">
+                    <a data-fancybox="works" href="line/12b.jpg"><img src="line/12b.jpg" class="mb20 mt20" width="270px"></a>
+                    <p class="wkz-font-16 whiteF mb20" style="font-weight: 300;text-transform: none">ГЛЯНЦЕВЫЕ ПОТОЛКИ</p>
+                </div>
+
+            </div>
+            <div class="row pad" align="center">
+                <div class="col-md-3 col-sm-12 col-xs-12">
+                    <a data-fancybox="works" href="line/1b.jpg"><img src="line/1b.jpg" class="mb20 mt20" width="270px"></a>
+                    <p class="wkz-font-16 whiteF mb20" style="font-weight: 300;text-transform: none">МАТОВЫЕ ПОТОЛКИ</p>
+                </div>
+
+
+                <div class="col-md-3 col-sm-12 col-xs-12">
+                    <a data-fancybox="works" href="line/3b.jpg"><img src="line/3b.jpg" class="mb20 mt20" width="270px"></a>
+                    <p class="wkz-font-16 whiteF mb20" style="font-weight: 300;text-transform: none">ЗВЕЗДНОЕ НЕБО</p>
+                </div>
+
+                <div class="col-md-3 col-sm-12 col-xs-12">
+                    <a data-fancybox="works" href="line/4b.jpg"><img src="line/4b.jpg" class="mb20 mt20" width="270px"></a>
+                    <p class="wkz-font-16 whiteF mb20" style="font-weight: 300;text-transform: none">DOUBLE VISION (3D)</p>
+                </div>
+                <div class="col-md-3 col-sm-12 col-xs-12">
+                    <a data-fancybox="works" href="line/5b.jpg"><img src="line/5b.jpg" class="mb20 mt20" width="270px"></a>
+                    <p class="wkz-font-16 whiteF mb20" style="font-weight: 300;text-transform: none">РЕЗНЫЕ ПОТОЛКИ APPLY</p>
+                </div>
+            </div>
+
+            <div class="row pad" align="center">
+
+
+
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                    <a data-fancybox="works" href="line/6b.jpg"><img src="line/6b.jpg" class="mb20 mt20" width="270px"></a>
+                    <p class="wkz-font-16 whiteF mb20" style="font-weight: 300;text-transform: none">СВЕТОВЫЕ ЛИНИИ</p>
+                </div>
+
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                    <a data-fancybox="works" href="line/7b.jpg"><img src="line/7b.jpg" class="mb20 mt20" width="270px"></a>
+                    <p class="wkz-font-16 whiteF mb20" style="font-weight: 300;text-transform: none">ПАРЯЩИЕ ПОТОЛКИ</p>
+                </div>
+
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                    <a data-fancybox="works" href="line/8b.jpg"><img src="line/8b.jpg" class="mb20 mt20" width="270px"></a>
+                    <p class="wkz-font-16 whiteF mb20" style="font-weight: 300;text-transform: none">ФОТОПЕЧАТЬ НА ПОТОЛКАХ</p>
+                </div>
+
+            </div>
+
+
+
+            <div class="row pad" align="center">
+
+                <div class="col-md-12 col-xs-12">
+                    <div class="wkz-font-20 bold font-up whiteF">Мы поможем Вам воплотить мечту в реальность! </div>
+                    <p class="wkz-font-16 mb10 " style="color: #fff; font-weight: 300;text-transform: none">Закажите натяжные потолки у нашей компании.</p>
+
+                    <div align="center">
+                        <a href="tel:87073770896" target="_blank" class="btn btn-lg buttonz mt10 mb10">
+                            <p class="wkz-font-16" style="font-weight: 300;">Позвонить +7 (747) 557 1973</p>
+                        </a>
+                    </div>
+
+                    <div align="center">
+                        <a href="tel:87052400203" target="_blank" class="btn btn-lg buttonz mt10 mb10">
+                            <p class="wkz-font-14 " style="font-weight: 300;">Заказать</p>
+                        </a>
+                    </div>
+
+
+                    <p class="wkz-font-14 whiteF" style="font-weight: 300;text-transform: none">Приедим к Вам, и сделаем замеры!</p>
+                    <p class="wkz-font-12 whiteF font-weight-light">Выезд в область по договоренности.</p>
+
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+
+    <section id="works2" class="pb100 pt100">
+        <div class="container">
+            <div class="row pad" align="center">
+                <div class="col-md-12 col-xs-12">
+                    <div class="wkz-font-25 bold font-up">Дополнительные услуги:</div>
+
+                </div>
+            </div>
+
+            <div class="row pad" align="center">
+                <div class="col-md-6 col-sm-12 col-xs-12">
+                    <img src="line/p1.jpg" class="mb20 mt10" width="270px">
+                    <div class="wkz-font-14 bold font-up mb10">Ремонт натяжного потолка</div>
+
+                </div>
+
+
+                <div class="col-md-6 col-sm-12 col-xs-12">
+                    <img src="line/p3.jpg" class="mb20 mt10" width="270px">
+                    <div class="wkz-font-14 bold font-up mb10">Слив воды с натяжного потолка</div>
+                    <p class="wkz-font-16" style="font-weight: 300;text-transform: none"></p>
+                </div>
+
+
+            </div>
+
+
+
+
+
+
+        </div>
+
+    </section>
+
+
+
+
+    <section id="price" class="d-xs-none d-md-block pt40 pb70">
+        <div class="container mt50">
+            <div class="row">
+                <div class="col-md-12" align="center">
+                    <h3 class="bold whiteF">Остались вопросы?</h3>
+                    <p class="whiteF">Позвоните или напишите в Whatsapp, мы найдем решение для Вас!</p>
+                    <div>
+                        <a href="tel:87073770896" target="_blank" class="btn btn-lg buttonz mt30 ">
+                            <p class="wkz-font-20" style="font-weight: 300;">Позвонить +7 (747) 557 1973</p>
+                        </a>
+                    </div>
+                    <div align="center">
+                        <a href="tel:87052400203" target="_blank" class="btn btn-lg buttonz mt10 mb10">
+                            <p class="wkz-font-20" style="font-weight: 300;">Заказать</p>
+                        </a>
+                    </div>
+                    <p class="wkz-font-18 font-weight-light whiteF mt10">Приедим к Вам, и сделаем замеры бесплатно! </p>
+                    <p class="wkz-font-12 font-weight-light whiteF mt5">Выезд в область по договоренности</p>
+                </div>
+
+                <div class="col-md-12 mt100" align="center">
+                    <img src="logo.png" width="150px">
+                    <p class="gray wkz-font-12">2021 &copy; Все права защищены.</p>
+
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="price" class="d-none d-xs-block pt40 pb70">
+
+        <div class="container mt20">
+            <div class="row">
+                <div class="col-md-12" align="center">
+                    <h4 class="bold whiteF">Остались вопросы?</h4>
+                    <p class="wkz-font-14 whiteF">Позвоните или напишите в Whatsapp, мы найдем решение для Вас!</p>
+
+                    <div align="center">
+                        <a href="tel:874745571973" target="_blank" class="btn btn-lg buttonz mt30">
+                            <p class="wkz-font-14" style="font-weight: 300;">Позвонить +7 (707) 377 0896</p>
+                        </a>
+                    </div>
+                    <div align="center">
+                        <a href="tel:87475571973" target="_blank" class="btn btn-lg buttonz mt10 mb10">
+                            <p class="wkz-font-14" style="font-weight: 300;">Заказать</p>
+                        </a>
+                    </div>
+                    <p class="wkz-font-14 font-weight-light whiteF mt10">Приедим к Вам, и сделаем замеры бесплатно! </p>
+                    <p class="wkz-font-12 font-weight-light whiteF mt10">Выезд в область по договоренности</p>
+                </div>
+
+                <div class="col-md-12 mt30" align="right">
+                    <img src="rlogo.png" width="150px">
+                    <p class="gray wkz-font-12"> 2021 &copy; Все права защищены.</p>
+
+
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+    <script src="js/jquery-3.2.1.min.js"></script>
+    <link rel="stylesheet" href="css/jquery.fancybox.min.css" defer/>
+    <script src="js/jquery.fancybox.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+
+    <script type="text/javascript">
+        $(function() {
+            $('a[href^="#"]').click(function() {
+                var target = $(this).attr('href');
+                $('html, body').animate({
+                    scrollTop: $(target).offset().top
+                }, 800); //800 - длительность скроллинга в мс
+                return false;
+            });
+        });
+    </script>
+
+
+
+</body>
+
+</html>
